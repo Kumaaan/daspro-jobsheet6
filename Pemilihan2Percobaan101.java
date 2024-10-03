58855441f34a3f545c3aba387b7eed35f2709c92
Adi Luhung <luhungadi571@gmail.com>
2024-10-03 20:59:10 +0700

    jobsheet5

---
+++ Pemilihan2Percobaan101.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan101 {
+    public static void main(String[] args) {
+        Scanner Absen = new Scanner(System.in);
+
+        int tahun = 0;
+
+        System.out.print("Masukkan Tahun : ");
+        tahun = Absen.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if((tahun % 100) != 0)
+                System.out.println("Tahun Kabisat");
+        } else
+            System.out.println("Bukan Tahun Kabisat");
+
+        Absen.close();
+    }
+}
